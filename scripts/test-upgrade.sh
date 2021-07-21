48167fff51c0d3a71071e179a03622e28be79c53
Shane Utt <shaneutt@linux.com>
2021-07-21 13:20:30 -0400

    feat: add generic upgrade test script
    This script takes a TAG and tests upgrading to that
    tag from whatever version is defined as the default
    in the local values.yaml or image latest.

---
+++ scripts/test-upgrade.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+# TAG=<OPTIONAL TAG> ./test-upgrade.sh
+#
+# This script (by default) tests against the `next` branch of the KIC to ensure
+# that chart upgrades work against the latest work being done there.
+#
+# Optionally, the script can be flagged to test upgrades from the current default
+# tag (provided by the chart and values.yaml) to any tag specified.
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Environment Variables
+# ------------------------------------------------------------------------------
+
+if [[ -z $TEST_ENV_NAME ]]
+then
+    TEST_ENV_NAME="kong-charts-tests"
+fi
+
+if [[ -z $TAG ]]
+then
+    TAG="next-railgun"
+fi
+
+if [[ -z $RELEASE_NAME ]]
+then
+    RELEASE_NAME="chart-tests-upgrade-compat"
+fi
+
+if [[ -z $RELEASE_NAMESPACE ]]
+then
+    RELEASE_NAMESPACE="$(uuidgen)"
+fi
+
+# ------------------------------------------------------------------------------
+# Shell Configuration
+# ------------------------------------------------------------------------------
+
+set -euo pipefail
+
+# ------------------------------------------------------------------------------
+# Kubernetes Cluster Setup
+# ------------------------------------------------------------------------------
+
+kubectl cluster-info --context kind-${TEST_ENV_NAME} 1>/dev/null
+KUBERNETES_VERSION="$(kubectl version -o json | jq -r '.serverVersion.gitVersion')"
+
+# ------------------------------------------------------------------------------
+# Setup Chart Cleanup - Kubernetes Ingress Controller
+# ------------------------------------------------------------------------------
+
+function cleanup() {
+    echo "INFO: cleaning up helm release ${RELEASE_NAME}"
+    helm delete --namespace ${RELEASE_NAMESPACE} ${RELEASE_NAME}
+    kubectl delete namespace ${RELEASE_NAMESPACE}
+    kubectl delete clusterrole ${RELEASE_NAME}-kong
+    exit 1
+}
+
+trap cleanup SIGINT SIGKILL
+
+# ------------------------------------------------------------------------------
+# Deploy Chart - Kubernetes Ingress Controller
+# ------------------------------------------------------------------------------
+
+cd charts/kong/
+echo "INFO: installing chart as release ${RELEASE_NAME} to namespace ${RELEASE_NAMESPACE}"
+helm install --create-namespace --namespace ${RELEASE_NAMESPACE} ${RELEASE_NAME} ./
+
+# ------------------------------------------------------------------------------
+# Test Chart - Kubernetes Ingress Controller
+# ------------------------------------------------------------------------------
+
+echo "INFO: running helm tests for all charts on Kubernetes ${KUBERNETES_VERSION}"
+helm test --namespace ${RELEASE_NAMESPACE} ${RELEASE_NAME}
+
+# ------------------------------------------------------------------------------
+# Upgrade Chart Image
+# ------------------------------------------------------------------------------
+
+echo "INFO: upgrading the helm chart to image tag ${TAG}"
+helm upgrade --namespace ${RELEASE_NAMESPACE} --set ingressController.image.tag=${TAG} ${RELEASE_NAME} ./
+
+# ------------------------------------------------------------------------------
+# Test Upgraded Chart
+# ------------------------------------------------------------------------------
+
+echo "INFO: running helm tests for all charts on Kubernetes ${KUBERNETES_VERSION}"
+helm test --namespace ${RELEASE_NAMESPACE} ${RELEASE_NAME}
