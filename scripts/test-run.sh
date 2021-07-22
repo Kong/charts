eefb18542fff06c680c6aae7762a67b3de0de640
Shane Utt <shaneutt@linux.com>
2021-07-22 14:37:28 -0400

    chore: test script cleanup
    This removes the cleanup functions (which were
    mainly helpful for debugging when developing the
    tests and running them manually) which will now
    be covered by the test env cleanup script, and
    also does some cleanup of some of the vars in
    the scripts for consistency and configurability.

--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -12,63 +12,34 @@
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# Environment Variables
-# ------------------------------------------------------------------------------
-
-TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
-TAG="${TAG:-default}"
-
-# ------------------------------------------------------------------------------
-# Shell Configuration
+# Configuration
 # ------------------------------------------------------------------------------
 
 set -euo pipefail
 
-# ------------------------------------------------------------------------------
-# Kubernetes Cluster Setup
-# ------------------------------------------------------------------------------
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${SCRIPT_DIR}/.."
 
-kubectl cluster-info --context kind-"${TEST_ENV_NAME}" 1>/dev/null
-KUBERNETES_VERSION="$(kubectl version -o json | jq -r '.serverVersion.gitVersion')"
-
-# TODO: this is a temporary shim to deploy the v1beta1.UDPIngress CRD and it will
-#       be removed in a follow up issue once v1 CRDs are supported by the chart.
-#
-#       See: https://github.com/Kong/charts/issues/391
-kubectl apply -f  https://raw.githubusercontent.com/Kong/kubernetes-ingress-controller/next/railgun/config/crd/bases/configuration.konghq.com_udpingresses.yaml
-
-# ------------------------------------------------------------------------------
-# Setup Chart Cleanup - Kubernetes Ingress Controller
-# ------------------------------------------------------------------------------
-
-RELEASE_NAME="chart-tests-${RANDOM}"
-RELEASE_NAMESPACE="$(uuidgen)"
-
-function cleanup() {
-    echo "INFO: cleaning up helm release ${RELEASE_NAME}"
-    helm delete --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}"
-    kubectl delete namespace "${RELEASE_NAMESPACE}"
-    kubectl delete clusterrole "${RELEASE_NAME}"-kong
-    exit 1
-}
-
-trap cleanup SIGINT
+TAG="${TAG:-default}"
+RELEASE_NAME="${RELEASE_NAME:-chart-tests}"
+RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-$(uuidgen)}"
+TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
+KUBECTL="kubectl --context kind-${TEST_ENV_NAME}"
+KUBERNETES_VERSION="$($KUBECTL version -o json | jq -r '.serverVersion.gitVersion')"
 
 # ------------------------------------------------------------------------------
 # Deploy Chart - Kubernetes Ingress Controller
 # ------------------------------------------------------------------------------
 
-cd charts/kong/
-
 if [[ "${TAG}" == "default" ]]
 then
     echo "INFO: installing chart as release ${RELEASE_NAME} to namespace ${RELEASE_NAMESPACE}"
     helm install --create-namespace --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
-	--set deployment.test.enabled=true ./
+        --set deployment.test.enabled=true charts/kong/
 else
     echo "INFO: installing chart as release ${RELEASE_NAME} with controller tag ${TAG} to namespace ${RELEASE_NAMESPACE}"
     helm install --create-namespace --namespace "${RELEASE_NAMESPACE}" \
-        --set ingressController.image.tag="${TAG}" "${RELEASE_NAME}" --set deployment.test.enabled=true ./
+        --set ingressController.image.tag="${TAG}" "${RELEASE_NAME}" --set deployment.test.enabled=true charts/kong/
 fi
 
 # ------------------------------------------------------------------------------
--- scripts/test-v2-deploy.sh
+++ scripts/test-v2-deploy.sh
@@ -14,9 +14,13 @@
 # Configuration
 # ------------------------------------------------------------------------------
 
-TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
-
 set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${SCRIPT_DIR}/.."
+
+
+TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
 
 # ------------------------------------------------------------------------------
 # Find and Test Latest V2 Tag
@@ -26,4 +30,4 @@ BASE="kong/kubernetes-ingress-controller"
 TAG="$(curl -s https://api.github.com/repos/${BASE}/releases | jq -r '.[] | select(.tag_name | test("^2.[0-9]+.[0-9]+")) | .name' | head -1)"
 
 echo "INFO: running tests for ${TAG}"
-TEST_ENV_NAME=${TEST_ENV_NAME} RELEASE_NAME="chart-test-v2-deploy" TAG=${TAG} ./scripts/test-run.sh
+TEST_ENV_NAME=${TEST_ENV_NAME} RELEASE_NAME="chart-test-v2-deploy" TAG=${TAG} "${SCRIPT_DIR}/test-run.sh"
