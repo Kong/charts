22ad551ab9a477369a6007dd0b2801d28f5eae2b
Michael Heap <m@michaelheap.com>
2023-05-24 17:38:38 +0100

    Add ingress chart tests

--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -22,10 +22,21 @@ cd "${SCRIPT_DIR}/.."
 
 TAG="${TAG:-default}"
 RELEASE_NAME="${RELEASE_NAME:-chart-tests}"
-RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-$(uuidgen)}"
+RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-$(uuidgen | tr "[:upper:]" "[:lower:]")}"
 TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
 KUBECTL="kubectl --context kind-${TEST_ENV_NAME}"
 KUBERNETES_VERSION="$($KUBECTL version -o json | jq -r '.serverVersion.gitVersion')"
+
+CONTROLLER_PREFIX=""
+ADDITIONAL_FLAGS=()
+
+# ------------------------------------------------------------------------------
+# Configure per-chart settings
+# ------------------------------------------------------------------------------
+if [[ "${CHART_NAME}" == "ingress" ]]; then
+  CONTROLLER_PREFIX="controller."
+  ADDITIONAL_FLAGS+=("--set ${CONTROLLER_PREFIX}ingressController.gatewayDiscovery.adminApiService.name=${RELEASE_NAME}-gateway-admin ")
+fi
 
 # ------------------------------------------------------------------------------
 # Deploy Kuma configuration and test namespace
@@ -62,29 +73,33 @@ metadata:
 # Deploy Chart - Kubernetes Ingress Controller
 # ------------------------------------------------------------------------------
 
-if [[ "${TAG}" == "default" ]]
+TAG_MESSAGE=""
+if [[ "${TAG}" != "default" ]]
 then
-    echo "INFO: installing chart as release ${RELEASE_NAME} to namespace ${RELEASE_NAMESPACE}"
-    helm install --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
-        --set deployment.test.enabled=true \
-        --set ingressController.env.feature_gates="GatewayAlpha=true" \
-        --set ingressController.env.anonymous_reports="false" \
-        charts/kong/
-else
-    echo "INFO: installing chart as release ${RELEASE_NAME} with controller tag ${TAG} to namespace ${RELEASE_NAMESPACE}"
-    helm install --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
-        --set ingressController.image.tag="${TAG}" \
-        --set ingressController.env.feature_gates="GatewayAlpha=true" \
-        --set ingressController.env.anonymous_reports="false" \
-        --set deployment.test.enabled=true \
-        charts/kong/
+  TAG_MESSAGE="with controller tag ${TAG} "
+  ADDITIONAL_FLAGS+=("--set ${CONTROLLER_PREFIX}ingressController.image.tag=${TAG} ");
 fi
+
+# Configure values for all tests
+# Enable Gateway API
+ADDITIONAL_FLAGS+=("--set ${CONTROLLER_PREFIX}ingressController.env.feature_gates=GatewayAlpha=true")
+# Tests should not show up in reporting
+ADDITIONAL_FLAGS+=("--set ${CONTROLLER_PREFIX}ingressController.env.anonymous_reports=false")
+
+echo "INFO: installing chart as release ${RELEASE_NAME} ${TAG_MESSAGE}to namespace ${RELEASE_NAMESPACE}"
+set -x
+# shellcheck disable=SC2048,SC2086
+helm install --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
+    --set deployment.test.enabled=true \
+    ${ADDITIONAL_FLAGS[*]} \
+    "charts/${CHART_NAME}"
+set +x
 
 # ------------------------------------------------------------------------------
 # Test Chart - Kubernetes Ingress Controller
 # ------------------------------------------------------------------------------
 
-echo "INFO: running helm tests for all charts on Kubernetes ${KUBERNETES_VERSION}"
+echo "INFO: running helm tests for ${CHART_NAME} chart on Kubernetes ${KUBERNETES_VERSION}"
 helm test --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}"
 
 # ------------------------------------------------------------------------------
--- scripts/test-upgrade.sh
+++ scripts/test-upgrade.sh
@@ -20,23 +20,36 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SCRIPT_DIR}/.."
 
 TAG="${TAG:-next-railgun}"
-EFFECTIVE_TAG="2.0.0"
+EFFECTIVE_TAG="99.0.0"
 RELEASE_NAME="${RELEASE_NAME:-chart-tests-upgrade-compat}"
 RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-$(uuidgen)}"
 TEST_ENV_NAME="${TEST_ENV_NAME:-kong-charts-tests}"
 KUBECTL="kubectl --context kind-${TEST_ENV_NAME}"
 KUBERNETES_VERSION="$($KUBECTL version -o json | jq -r '.serverVersion.gitVersion')"
 
+CONTROLLER_PREFIX=""
+ADDITIONAL_FLAGS=()
+
+# ------------------------------------------------------------------------------
+# Configure per-chart settings
+# ------------------------------------------------------------------------------
+if [[ "${CHART_NAME}" == "ingress" ]]; then
+  CONTROLLER_PREFIX="controller."
+  ADDITIONAL_FLAGS+=("--set ${CONTROLLER_PREFIX}ingressController.gatewayDiscovery.adminApiService.name=\"${RELEASE_NAME}-gateway-admin\" ")
+fi
+
 # ------------------------------------------------------------------------------
 # Deploy Chart - Kubernetes Ingress Controller
 # ------------------------------------------------------------------------------
 
 echo "INFO: installing chart as release ${RELEASE_NAME} to namespace ${RELEASE_NAMESPACE}"
+set -x
+# shellcheck disable=SC2048,SC2086
 helm install --create-namespace --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
-    --set ingressController.env.anonymous_reports="false" \
-    --set deployment.test.enabled=true \
-    charts/kong/
-
+    --set ${CONTROLLER_PREFIX}ingressController.env.anonymous_reports="false" \
+    --set deployment.test.enabled=true ${ADDITIONAL_FLAGS[*]} \
+    "charts/${CHART_NAME}"
+set +x
 # ------------------------------------------------------------------------------
 # Test Chart - Kubernetes Ingress Controller
 # ------------------------------------------------------------------------------
@@ -49,18 +62,20 @@ helm test --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}"
 # ------------------------------------------------------------------------------
 
 echo "INFO: upgrading the helm chart to image tag ${TAG}"
+set -x
+# shellcheck disable=SC2048,SC2086
 helm upgrade --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
-    --set ingressController.image.tag="${TAG}" \
-    --set deployment.test.enabled=true \
-    --set ingressController.env.anonymous_reports="false" \
-    --set ingressController.image.effectiveSemver="${EFFECTIVE_TAG}" \
-    charts/kong/
-
+    --set ${CONTROLLER_PREFIX}ingressController.image.tag="${TAG}" \
+    --set deployment.test.enabled=true ${ADDITIONAL_FLAGS[*]} \
+    --set ${CONTROLLER_PREFIX}ingressController.env.anonymous_reports="false" \
+    --set ${CONTROLLER_PREFIX}ingressController.image.effectiveSemver="${EFFECTIVE_TAG}" \
+    "charts/${CHART_NAME}"
+set +x
 # ------------------------------------------------------------------------------
 # Test Upgraded Chart
 # ------------------------------------------------------------------------------
 
-echo "INFO: running helm tests for all charts on Kubernetes ${KUBERNETES_VERSION}"
+echo "INFO: running helm tests for ${CHART_NAME} chart on Kubernetes ${KUBERNETES_VERSION}"
 helm test --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}"
 
 
