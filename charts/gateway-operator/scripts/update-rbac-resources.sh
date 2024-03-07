5e5c0eb1646fa4bd8d3405fa5faeaa93b53fbadb
Jintao Zhang <zhangjintao9020@gmail.com>
2024-03-08 02:45:25 +0800

    feat: add gateway-operator helm chart (#1020)
    Add a base chart for the Kong Gateway Operator.
    ---------
    
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>
    Co-authored-by: Travis Raines <571832+rainest@users.noreply.github.com>

---
+++ charts/gateway-operator/scripts/update-rbac-resources.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# 
+# cd kong/gateway-operator/config 
+# kustomize build rbac > /tmp/rbac-resources.yaml
+# sed -i 's/namespace: kong-system/namespace: {{ template "kong.namespace" . }}/g' /tmp/rbac-resources.yaml
+# sed -i 's/name: controller-manager$/name: {{ template "kong.serviceAccountName" . }}/g' /tmp/rbac-resources.yaml
+# sed -i 's/name: gateway-operator-manager-role/name: {{ template "kong.fullname" . }}-manager-role/g' /tmp/rbac-resources.yaml
+# sed -i 's/name: controller-manager-metrics-service/name: {{ template "kong.fullname" . }}-metrics-service/g' /tmp/rbac-resources.yaml
+# Then copy the contents of this file except for the Service Account resource using the following command
+# (head -n 11 PATH_OF_YOUR_CHARTS_REPO/charts/gateway-operator/templates/rbac-resources.yaml && tail -n +6 /tmp/rbac-resources.yaml) > /tmp/new-rbac-resources.yaml
+# mv /tmp/new-rbac-resources.yaml YOUR-PATH-OF-CHARTS/charts/gateway-operator/templates/rbac-resources.yaml
+# ------------------------------------------------------------------------------
+
+# this script will receive two arguments:
+# $1: the path to the kgo repository
+# $2: the path to the kong/charts repository
+
+set -euo pipefail
+
+if [ "$#" -ne 2 ]; then
+  echo "Error: You must provide exactly two arguments."
+  exit 1
+fi
+
+KGO_REPO_PATH=$1
+CHARTS_REPO_PATH=$2
+
+# check if the kgo repository path is empty
+if [ -z "$KGO_REPO_PATH" ]
+then
+  echo "The path to the kgo repository is required"
+  exit 1
+fi
+
+# check if the kong/charts repository path is empty
+if [ -z "$CHARTS_REPO_PATH" ]
+then
+  echo "The path to the kong/charts repository is required"
+  exit 1
+fi
+
+# create a function named update_rbac_resources
+function update_rbac_resources {
+  # build the kustomize resources
+  kustomize build $KGO_REPO_PATH/config/rbac > /tmp/rbac-resources.yaml
+
+  # replace the namespace
+  sed -i 's/namespace: kong-system/namespace: {{ template "kong.namespace" . }}/g' /tmp/rbac-resources.yaml
+
+  # replace the service account name
+  sed -i 's/name: controller-manager$/name: {{ template "kong.serviceAccountName" . }}/g' /tmp/rbac-resources.yaml
+
+  # replace the role name
+  sed -i 's/name: gateway-operator-manager-role/name: {{ template "kong.fullname" . }}-manager-role/g' /tmp/rbac-resources.yaml
+
+  # replace the metrics service name
+  sed -i 's/name: controller-manager-metrics-service/name: {{ template "kong.fullname" . }}-metrics-service/g' /tmp/rbac-resources.yaml
+
+  # replace the name of the resources
+  sed -i '/name: {{\|name: https/!s/name: /name: {{ template "kong.fullname" . }}-/g' /tmp/rbac-resources.yaml
+
+  # copy the contents of the file except for the Service Account resource
+  (head -n 4 $CHARTS_REPO_PATH/charts/gateway-operator/templates/rbac-resources.yaml && tail -n +6 /tmp/rbac-resources.yaml) > /tmp/new-rbac-resources.yaml
+
+  # move the new file to the charts directory
+  mv /tmp/new-rbac-resources.yaml $CHARTS_REPO_PATH/charts/gateway-operator/templates/rbac-resources.yaml
+}
+
+# call the update_rbac_resources function
+update_rbac_resources
+
