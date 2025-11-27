9c90deb4991793ac603aefed4086f3df61b6e113
Patryk Małek <pmalek@users.noreply.github.com>
2025-11-27 16:10:37 +0100

    chore: install helm with mise (#1439)
    * chore: install helm with mise
    
    * chore: remove duplicated KIC CRDs from gateway-operator KIC subchart
    
    * chore: set helm version to 3.19.2
    
    * chore: regenerate golden test files
    
    * chore: don't install kic CRD for KGO chart

--- scripts/test-env.sh
+++ scripts/test-env.sh
@@ -90,9 +90,6 @@ ktf 1>/dev/null
 if [[ "${CHART_NAME}" == "gateway-operator" ]]
 then
   ktf environments create --name "${TEST_ENV_NAME}" --addon metallb --kubernetes-version "${KUBERNETES_VERSION}"
-  GATEWAY_API_VERSION="v1.0.0"
-  # Install Kong specific CRDs
-  kubectl apply -k https://github.com/Kong/kubernetes-ingress-controller/config/crd
 else
   ktf environments create --name "${TEST_ENV_NAME}" --addon metallb --addon kuma --kubernetes-version "${KUBERNETES_VERSION}"
 fi
--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -60,7 +60,8 @@ then
   echo "INFO: installing chart as release ${RELEASE_NAME} ${TAG_MESSAGE}to namespace ${RELEASE_NAMESPACE}"
   set -x
   # shellcheck disable=SC2048,SC2086
-  helm install --create-namespace --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
+  helm install \
+      --create-namespace --namespace "${RELEASE_NAMESPACE}" "${RELEASE_NAME}" \
       --set test.enabled=true \
       ${ADDITIONAL_FLAGS[*]} \
       "charts/${CHART_NAME}"
